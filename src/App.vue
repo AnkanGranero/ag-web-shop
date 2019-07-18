bddab4144b2a8588c9679638098c715003e89008
Andreas Graner <Andreas@graner.me>
2019-07-18 12:32:58 +0200

    added in Gallery template and the computed products

--- src/App.vue
+++ src/App.vue
@@ -26,4 +26,7 @@
     }
   }
 }
+li {
+  list-style: none;
+}
 </style>
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,5 +1,24 @@
  <template>
   <div class="galleryWrapper">
     <h1>Gallery</h1>
+    <ul v-for="product in products" :key="product.id">
+      <h2>{{ product.name}}</h2>
+      <!--  <img :src="product.src" /> -->
+      <li>Size: {{ product.size}}</li>
+      <li>Quantity: {{ product.quantity}}</li>
+    </ul>
   </div>
 </template>
+<script>
+export default {
+  name: "gallery",
+  computed: {
+    products() {
+      return this.$store.state.products;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
