d9cfeaa989e46c3b168a8cfef6fb3ef996ad9a32
Andreas Graner <Andreas@graner.me>
2019-07-18 12:55:47 +0200

    implemented mixin imagePath in Gallery

--- src/mixins/imagePath.js
+++ src/mixins/imagePath.js
@@ -1,7 +1,7 @@
 export const imagePath = {
     methods: {
         makeImagePath(product) {
-
+            console.log("im in", product)
             return require(`../assets/images/products/${product.images[0]}`);
         }
     }
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -3,20 +3,22 @@
     <h1>Gallery</h1>
     <ul v-for="product in products" :key="product.id">
       <h2>{{ product.name}}</h2>
-      <!--  <img :src="product.src" /> -->
+      <img :src="makeImagePath(product)" />
       <li>Size: {{ product.size}}</li>
       <li>Quantity: {{ product.quantity}}</li>
     </ul>
   </div>
 </template>
 <script>
+import { imagePath } from "@/mixins/imagePath.js";
 export default {
   name: "gallery",
   computed: {
     products() {
       return this.$store.state.products;
     }
-  }
+  },
+  mixins: [imagePath]
 };
 </script>
 
